87a9ccf990455dc9370938262e35cad46ff7880c
facingsouth <xin.d.liu@gmail.com>
2015-09-04 09:15:18 -0700

    start $.ajax

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  // var xhr = new XMLHttpRequest();
+  // xhr.addEventListener( "load", function(){
+  //     console.log( this.responseText )
+  // });
+  // xhr.open("POST", "http://reqr.es/api/users", true);
+  // xhr.send("first_name=foo&last_name=bar");
+
+  // var xhr = new XMLHttpRequest();
+  // xhr.addEventListener( "load", function(){
+  //     console.log( this.responseText )
+  // });
+  // xhr.open("GET", "http://reqr.es/api/users", true);
+  // xhr.send("id=2");
+})
+
+var MyjQuery = MyjQuery || {}
+
+MyjQuery.$ = (function(){
+
+  function ajax(arg) {
+    if (typeof arg !== "object")
+    var xhr = new XMLHttpRequest();
+  }
+
+})();
